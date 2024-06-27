5da185575a85d4ab7a64f53efeb7769285a78b48
unknown <zienzidan41@gmail.com>
2024-06-27 19:56:01 +0700

    Update tugas 4

---
+++ Tugas 4/run_process.sh
@@ -0,0 +1,9 @@
+#jalankan 5 server_thread
+
+python3 server_process_pool_http.py 8880 &
+python3 server_process_pool_http.py 8881 &
+python3 server_process_pool_http.py 8882 &
+python3 server_process_pool_http.py 8883 &
+
+
+
---
+++ Tugas 4/runserver.sh
@@ -0,0 +1,9 @@
+#jalankan 5 async_server
+
+python3 async_server.py 7002 &
+python3 async_server.py 7003 &
+python3 async_server.py 7004 &
+python3 async_server.py 7005 &
+
+
+
